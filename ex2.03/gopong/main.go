ababf0b8837f8e13ebd58396750d399bc7878fe1
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-12-05 17:09:12 +0200

    ex2.03

---
+++ ex2.03/gopong/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync/atomic"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+	var counter uint32 = 0
+
+	http.HandleFunc("/pingpong", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		count := atomic.AddUint32(&counter, 1)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Ping / Pongs: %d", count-1)))
+	})
+
+	fmt.Printf("server running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
